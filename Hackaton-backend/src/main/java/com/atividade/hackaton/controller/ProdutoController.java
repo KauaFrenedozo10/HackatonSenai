078b0e424ff577705a79419212cc9e2869280481
RafaelAlves98 <sevla.rafa98@gmail.com>
2025-06-04 16:56:42 -0300

    Commitando

---
+++ Hackaton-backend/src/main/java/com/atividade/hackaton/controller/ProdutoController.java
@@ -0,0 +1,47 @@
+package com.atividade.hackaton.controller;
+
+import com.atividade.hackaton.dto.ProdutoDTO;
+import com.atividade.hackaton.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @PostMapping
+    public ResponseEntity<ProdutoDTO> criarProduto(@RequestBody ProdutoDTO produtoDTO) {
+        ProdutoDTO produtoCriado = produtoService.criarProduto(produtoDTO);
+        return ResponseEntity.status(201).body(produtoCriado);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoDTO>> listarProdutos() {
+        List<ProdutoDTO> produtos = produtoService.listarProdutos();
+        return ResponseEntity.ok(produtos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoDTO> obterProdutoPorId(@PathVariable Long id) {
+        ProdutoDTO produto = produtoService.obterProdutoPorId(id);
+        return ResponseEntity.ok(produto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProdutoDTO> atualizarProduto(@PathVariable Long id, @RequestBody ProdutoDTO produtoDTO) {
+        ProdutoDTO produtoAtualizado = produtoService.atualizarProduto(id, produtoDTO);
+        return ResponseEntity.ok(produtoAtualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarProduto(@PathVariable Long id) {
+        produtoService.deletarProduto(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ Hackaton-backend/src/main/java/com/atividade/hackaton/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.atividade.hackaton.repository;
+
+import com.atividade.hackaton.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
